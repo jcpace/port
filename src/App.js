61e5a327b6d00f9db6b866985cf2aab0b06e25da
Joshua Pace <joshpace@gmail.com>
2018-02-14 18:36:48 -0800

    add nav

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <nav className='navigation'>
+        <a href="/html/">work</a> |
+        <a href="/css/">about</a> |
+        <a href="/js/">contact</a> |
+      </nav>
        <p className='name'>Joshua Pace</p>
       </div>
     );
