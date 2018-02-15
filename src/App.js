86c92f5fd5b582b71755b637bca1444aed8f33f8
Joshua Pace <joshpace@gmail.com>
2018-02-14 18:33:23 -0800

    adjust size

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <h2 className='name'>Joshua Pace</h2>
+       <p className='name'>Joshua Pace</p>
       </div>
     );
   }
