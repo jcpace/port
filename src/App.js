ac90d267d3354081197df7ceab57166da3bdef7c
Joshua Pace <joshpace@gmail.com>
2018-02-15 22:29:35 -0800

    add transition to name el

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header / >
-       <p className='name'>Joshua Pace</p>
-       <Footer />
+        <Header />
+        <div className='name'>Joshua Pace
+          <div className="hiddentext"><p>I am a full stack web developer; I learn something new everyday. I hope you do too.</p></div>
+        </div>
+        <Footer />
       </div>
     );
   }
