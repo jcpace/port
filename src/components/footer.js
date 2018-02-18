7b0e97151283d7bf3d4ba344e0afae34dbf214f3
Joshua Pace <joshpace@gmail.com>
2018-02-17 17:32:25 -0800

    rm material icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,16 +4,10 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer className="social-footer">
-    <a href="https://www.instagram.com/_.jpace/" className="fa fa-instagram"></a>
-    <a href="mailto:joshpace@gmail.com" className="material-icons">email</a>
-    <a href='https://www.github.com/jcpace' className="fa fa-github"></a>
-
-
- 
-
-    
-
-</footer>
+      <a href="mailto:joshpace@gmail.com" className="fa fa-envelope"></a>
+      <a href="https://www.instagram.com/_.jpace/" className="fa fa-instagram"></a>
+      <a href='https://www.github.com/jcpace' className="fa fa-github"></a>
+  </footer>
 
   );
 }
