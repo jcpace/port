b9f3cc2481884f3916a82de156bd21f8df134ced
Joshua Pace <joshpace@gmail.com>
2018-02-18 12:02:48 -0800

    lint

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './footer.css';
 
-const Footer = () => {
-  return (
-    <footer className="social-footer">
-      <a href="mailto:joshpace@gmail.com" className="fa fa-envelope"></a>
-      <a href="https://www.instagram.com/_.jpace/" className="fa fa-instagram"></a>
-      <a href='https://www.github.com/jcpace' className="fa fa-github"></a>
-      <a href='https://www.linkedin.com/in/joshuacpace/' className="fa fa-linkedin"></a>
+const Footer = () => (
+  <footer className="social-footer">
+    <a href="mailto:joshpace@gmail.com" className="fa fa-envelope" />
+    <a href="https://www.instagram.com/_.jpace/" className="fa fa-instagram" />
+    <a href="https://www.github.com/jcpace" className="fa fa-github" />
+    <a href="https://www.linkedin.com/in/joshuacpace/" className="fa fa-linkedin" />
   </footer>
-
-  );
-};
+);
 
 export default Footer;
