203db58d3d1faad5cd0337d414383985e17b7ea8
Joshua Pace <joshpace@gmail.com>
2018-02-17 17:41:31 -0800

    add linkedin contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
       <a href="mailto:joshpace@gmail.com" className="fa fa-envelope"></a>
       <a href="https://www.instagram.com/_.jpace/" className="fa fa-instagram"></a>
       <a href='https://www.github.com/jcpace' className="fa fa-github"></a>
+      <a href='https://www.linkedin.com/in/joshuacpace/' className="fa fa-linkedin"></a>
   </footer>
 
   );
