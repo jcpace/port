8e4c43fc04851e90d216f05ef7f9e1684de42beb
Joshua Pace <joshpace@gmail.com>
2018-02-19 12:29:35 -0800

    styles

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,7 @@ import './about.css';
 const About = () => (
  <div id='about'>
      <div className="item" data-aos="zoom-in">Lorem ipsum dolor amet copper mug hashtag neutra fashion axe chicharrones DIY affogato. Sriracha four dollar toast gochujang, kale chips chartreuse umami direct trade.</div>
+     <a target='_blank' href='https://goo.gl/9PTUob' className=''>resume</a>
  </div>
 );
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,25 +3,24 @@ import './projects.css';
 
 const Projects = () => (
   <div id='projects'>
-    <p>work</p>
     <div className='wrapper'>
-    <div class="card">
-          <div class="card-header">
-            <h2 class="heading-2">Polished</h2>
-            <p class="subtitle">Open Source mobile app connecting users with independent manicure artists in their area.</p>
+    <div className="card">
+          <div className="card-header">
+            <h2 className="heading-2">Polished</h2>
+            <p className="subtitle">Open Source mobile app connecting users with independent manicure artists in their area.</p>
           </div>
-          <div class="card-description">
+          <div className="card-description">
             <p>Empowering independent artists</p>
           </div>
           <div class="card-tech">
-            <span class="tech">Angular 2</span>
-            <span class="tech">SCSS</span>
-            <span class="tech">Sequelize</span>
-            <span class="tech">Node.js</span>
+            <span className="tech">Angular 2</span>
+            <span className="tech">SCSS</span>
+            <span className="tech">Sequelize</span>
+            <span className="tech">Node.js</span>
           </div>
-          <div class="card-links">
-            <a href="http://www.brianadacoscos.com/" target="_blank" class="button">Visit Live</a>
-            <a href="https://github.com/jcpace/polished" target="_blank" class="button">GitHub Repo</a>
+          <div className="card-links">
+            <a href="http://www.brianadacoscos.com/" target="_blank" className="button">Visit Live</a>
+            <a href="https://github.com/jcpace/polished" target="_blank" className="button">GitHub Repo</a>
           </div>
         </div>
     
