b5d3ef2ffc34ef1ee3fe87c636e6a43fcc20835f
Joshua Pace <joshpace@gmail.com>
2018-02-18 19:39:14 -0800

    add polished card

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,7 +4,28 @@ import './projects.css';
 const Projects = () => (
   <div id='projects'>
     <p>work</p>
-    <i className="fa fa-react">react</i>
+    <div className='wrapper'>
+    <div class="card">
+          <div class="card-header">
+            <h2 class="heading-2">Polished</h2>
+            <p class="subtitle">Open Source mobile app connecting users with independent manicure artists in their area.</p>
+          </div>
+          <div class="card-description">
+            <p>Empowering independent artists</p>
+          </div>
+          <div class="card-tech">
+            <span class="tech">Angular 2</span>
+            <span class="tech">SCSS</span>
+            <span class="tech">Sequelize</span>
+            <span class="tech">Node.js</span>
+          </div>
+          <div class="card-links">
+            <a href="http://www.brianadacoscos.com/" target="_blank" class="button">Visit Live</a>
+            <a href="https://github.com/jcpace/polished" target="_blank" class="button">GitHub Repo</a>
+          </div>
+        </div>
+    
+    </div>
   </div>
   );
 
