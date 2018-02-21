ce8bba49d48933513a2c095d3be6f0616b3174b4
Joshua Pace <joshpace@gmail.com>
2018-02-20 16:15:34 -0800

    style on card

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText,
+} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
 const ProjectCard = () => (
@@ -12,18 +19,18 @@ const ProjectCard = () => (
     <CardMedia
       overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
     >
-      <img src="images/nature-600-337.jpg" alt="" />
+      <img src="../assets/mobile.jpg" alt="" />
     </CardMedia>
     <CardTitle title="Card title" subtitle="Card subtitle" />
     <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis
+      pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate
+      interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam
+      dui mauris, mattis quis lacus id, pellentesque lobortis odio.
     </CardText>
     <CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
+      <FlatButton label="View Live" />
+      <FlatButton label="Github Repo" />
     </CardActions>
   </Card>
 );
