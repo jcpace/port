8b1fda757a125a7aea0263a95aaf21a6d860151d
Joshua Pace <joshpace@gmail.com>
2018-02-24 21:06:19 -0800

    change header

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,6 @@ import React from 'react';
 import './header.css';
 
 const Header = () => (
-  // <nav className="navigation">
-  //   <a href="#about">about</a> |
-  //   <a href="#projects">work</a> |
-  //   <a href="#contact">contact</a>
-  // </nav>
   <div className="navigation">
     <ul>
       <a href="#about"><li><span className="header-span">about</span></li></a> |
