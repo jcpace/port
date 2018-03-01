456a41aa155a1998276f5092dd9a6c7fa83958b8
Joshua Pace <joshpace@gmail.com>
2018-02-28 21:28:32 -0800

    lint

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,30 +2,44 @@ import React from 'react';
 import './projects.css';
 
 const Projects = () => (
-  <div id='projects'>
-    <div className='wrapper'>
-    <div className="card">
-          <div className="card-header">
-            <h2 className="heading-2">Polished</h2>
-            <p className="subtitle">Open Source mobile app connecting users with independent manicure artists in their area.</p>
-          </div>
-          <div className="card-description">
-            <p>Empowering independent artists</p>
-          </div>
-          <div class="card-tech">
-            <span className="tech">Angular 2</span>
-            <span className="tech">SCSS</span>
-            <span className="tech">Sequelize</span>
-            <span className="tech">Node.js</span>
-          </div>
-          <div className="card-links">
-            <a href="http://www.brianadacoscos.com/" target="_blank" className="button">Visit Live</a>
-            <a href="https://github.com/jcpace/polished" target="_blank" className="button">GitHub Repo</a>
-          </div>
+  <div id="projects">
+    <div className="wrapper">
+        <div className="card">
+        <div className="card-header">
+          <h2 className="heading-2">Polished</h2>
+          <p className="subtitle">
+            Open Source mobile app connecting users with independent manicure
+            artists in their area.
+          </p>
         </div>
-    
+        <div className="card-description">
+          <p>Empowering independent artists</p>
+        </div>
+        <div className="card-tech">
+          <span className="tech">Angular 2</span>
+          <span className="tech">SCSS</span>
+          <span className="tech">Sequelize</span>
+          <span className="tech">Node.js</span>
+        </div>
+      <div className="card-links">
+          <a
+            href="http://www.brianadacoscos.com/"
+            target="_blank"
+            className="button"
+          >
+            Visit Live
+          </a>
+          <a
+            href="https://github.com/jcpace/polished"
+            target="_blank"
+            className="button"
+          >
+            GitHub Repo
+          </a>
+    </div>
+      </div>
     </div>
   </div>
-  );
+);
 
 export default Projects;
