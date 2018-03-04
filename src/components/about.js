e858cca757ad638926c6f774268b4795719b1233
Joshua Pace <joshpace@gmail.com>
2018-03-03 23:21:06 -0800

    adjust

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './about.css';
 const About = () => (
  <div id='about'>
-     <div className="item" data-aos="zoom-in">Lorem ipsum dolor amet copper mug hashtag neutra fashion axe chicharrones DIY affogato. Sriracha four dollar toast gochujang, kale chips chartreuse umami direct trade.</div>
+     <div className="item" data-aos="zoom-in">Lorem ipsum dolor amet copper mug hashtag neutra axe chicharrones DIY affogato. Sriracha four dollar toast gochujang, kale chips chartreuse umami direct trade.</div>
      <a target='_blank' href='https://goo.gl/9PTUob' className=''>resume</a>
  </div>
 );
