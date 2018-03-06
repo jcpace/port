5c4f462dc06995aadd75fc99d1189df95e45ba52
Joshua Pace <joshpace@gmail.com>
2018-03-05 21:39:39 -0800

    lint

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './about.css';
+
 const About = () => (
- <div id='about'>
-     <div className="item" data-aos="zoom-in">Lorem ipsum dolor amet copper mug hashtag neutra axe chicharrones DIY affogato. Sriracha four dollar toast gochujang, kale chips chartreuse umami direct trade.</div>
-     <a target='_blank' href='https://goo.gl/9PTUob' className=''>resume</a>
- </div>
+  <div id="about">
+    <div className="item" data-aos="zoom-in">
+      Lorem ipsum dolor amet copper mug hashtag neutra axe chicharrones DIY
+      affogato. Sriracha four dollar toast gochujang, kale chips chartreuse
+      umami direct trade.
+    </div>
+    <a target="_blank" href="https://goo.gl/9PTUob" className="">
+      resume
+    </a>
+  </div>
 );
 
 export default About;
